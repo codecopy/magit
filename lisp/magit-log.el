71792bb549b0e06ec02316882402206376305975
Jonas Bernoulli <jonas@bernoul.li>
2018-10-15 12:05:00 +0200

    Define magit-cherry-pick as a transient command

--- lisp/magit-log.el
+++ lisp/magit-log.el
@@ -914,7 +914,7 @@ to visit the commit at point.
 
 Type \\[magit-branch-popup] to see available branch commands.
 Type \\[magit-merge] to merge the branch or commit at point.
-Type \\[magit-cherry-pick-popup] to apply the commit at point.
+Type \\[magit-cherry-pick] to apply the commit at point.
 Type \\[magit-reset] to reset `HEAD' to the commit at point.
 
 \\{magit-log-mode-map}"
@@ -1529,7 +1529,7 @@ Type \\[magit-refresh] to refresh the current buffer.
 Type \\[magit-visit-thing] or \\[magit-diff-show-or-scroll-up] \
 to visit the commit at point.
 
-Type \\[magit-cherry-pick-popup] to apply the commit at point.
+Type \\[magit-cherry-pick] to apply the commit at point.
 
 \\{magit-cherry-mode-map}"
   :group 'magit-log
@@ -1587,7 +1587,7 @@ Type \\[magit-refresh] to refresh the current buffer.
 Type \\[magit-visit-thing] or \\[magit-diff-show-or-scroll-up] \
 to visit the commit at point.
 
-Type \\[magit-cherry-pick-popup] to apply the commit at point.
+Type \\[magit-cherry-pick] to apply the commit at point.
 Type \\[magit-reset] to reset `HEAD' to the commit at point.
 
 \\{magit-reflog-mode-map}"
